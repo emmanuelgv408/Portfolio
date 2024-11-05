efacf571f35ab2453b1e129dae868a12f83880ea
Emmanuel Guerrero Vega <emmanuelgv@live.com>
2024-11-05 10:58:04 -0800

    finished hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
+import {useTypewriter, Cursor} from 'react-simple-typewriter'
+
 
 
 function Hero() {
+
+  const [text] = useTypewriter ({
+    words: ['Front-End Developer', 'Student', 'Hot Wing Lover', 'Gym Addict'],
+    loop: {},
+    typeSpeed: 120,
+    deleteSpeed: 80
+  
+  
+  });
+
   return (
     <main className="flex flex-col min-w-full min-h-screen  justify-center space-y-[200px] ">
       <div className="flex flex-col space-y-8  ml-[150px]">
@@ -13,8 +25,16 @@ function Hero() {
        
         <p className="text-cyan text-7xl">
           Hey, <br />
-          I'm <span className="text-custom-gray">Emmanuel</span>
-        </p>
+          I'm <span className="text-white">
+            Emmanuel
+            </span>
+          <br /> I'm a {' '}
+          <span className="text-white">
+            {text}
+            </span>
+          <Cursor cursorColor="white"/>
+           
+        </p> 
 
         <button className="w-[150px] border rounded-md px-[24px] py-[10px] text-primary-text bg-cyan border-none hover:cursor-pointer">
           Contact
