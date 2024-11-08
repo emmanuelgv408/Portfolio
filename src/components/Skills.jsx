1cc3bdf10c0630e8a69fb2ab5c9e71663ddf9c8b
Emmanuel Guerrero Vega <emmanuelgv@live.com>
2024-11-07 16:45:17 -0800

    working on framer issue

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,8 +5,8 @@ import gitIcon from '../assets/git-icon.svg';
 import htmlIcon from '../assets/html-icon.svg';
 import jsIcon from '../assets/js-icon.svg';
 import tailwindIcon from '../assets/tailwind-icon.svg';
-import reactIcon from '../assets/react-icon.svg'
-import tsIcon from '../assets/ts-icon.svg'
+import reactIcon from '../assets/react-icon.svg';
+import tsIcon from '../assets/ts-icon.svg';
 
 function Skills() {
   const icons = [
@@ -15,14 +15,15 @@ function Skills() {
     { src: htmlIcon, name: 'HTML' },
     { src: jsIcon, name: 'JavaScript' },
     { src: tailwindIcon, name: 'Tailwind' },
-    
+    { src: reactIcon, name: 'React' },
+    { src: tsIcon, name: 'TypeScript' },
   ];
 
   return (
     <div className="h-screen bg-background text-cyan p-8 flex flex-col gap-3">
       <h1 className="text-3xl font-bold">Skills</h1>
 
-      <div className="h-[30%] bg-custom-grey flex overflow-hidden whitespace-nowrap">
+      <div className="h-[40%] bg-custom-grey flex overflow-hidden whitespace-nowrap">
         {Array(2).fill().map((_, i) => (
           <motion.div
             initial={{ x: '-100%' }}
@@ -32,17 +33,22 @@ function Skills() {
             className="flex items-center gap-6 py-2"
           >
             {icons.map((icon, index) => (
-              <div key={index} className="flex flex-col items-center">
-                <img src={icon.src} alt={`${icon.name} icon`} className="w-16 h-16 object-contain" />
+              <div key={index} className="flex flex-col items-center w-16 h-16">
+                <img
+                  src={icon.src}
+                  alt={`${icon.name} icon`}
+                  className="w-full h-full object-contain"
+                />
                 <p className="font-bold text-white">{icon.name}</p>
               </div>
             ))}
           </motion.div>
         ))}
       </div>
-      <div className=" flex justify-end">
-        <p className="text-3xl w-[70%] ">
-          I am a university student actively honing my technical skills both
+
+      <div className="flex justify-end">
+        <p className="text-3xl w-[70%]">
+           I am a university student actively honing my technical skills both
           through my academic coursework and personal projects. In addition to
           staying on track with my program, I dedicate time outside of my
           studies to explore and master new technologies and development tools.
@@ -53,7 +59,6 @@ function Skills() {
           developer.
         </p>
       </div>
-
     </div>
   );
 }
