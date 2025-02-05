3413b944f85e3ae72c9efd14fee4ce4159069a68
Emmanuel Guerrero Vega <emmanuelgv@live.com>
2025-02-04 22:38:33 -0800

    added mongodb to skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -8,6 +8,7 @@ import reactIcon from "../assets/react-icon.svg";
 import tsIcon from "../assets/ts-icon.svg";
 import expressIcon from "../assets/expressjs-icon.svg";
 import nodeIcon from "../assets/nodejs-icon.svg";
+import mongodbIcon from "../assets/mongodb-icon.svg"
 
 const skills = [
   { name: "React", icon: reactIcon },
@@ -19,13 +20,14 @@ const skills = [
   { name: "Node.js", icon: nodeIcon },
   { name: "Express.js", icon: expressIcon },
   { name: "Git", icon: gitIcon },
+  {name: "MongoDb", icon: mongodbIcon}
 ];
 
 function Skills() {
   return (
     <div className="bg-background text-cyan px-8 py-10">
       <h1 className="sm:text-2xl text-4xl font-extrabold underline text-center">Skills</h1>
-      <div className="grid grid-cols-2 sm:grid-cols-3 gap-9 mt-10">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-9 mt-10">
         {skills.map((skill) => (
           <div
             key={skill.name}
